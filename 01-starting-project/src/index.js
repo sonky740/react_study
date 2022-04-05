59c1de80b636e793f94aa30be21681b87dd0d75a
손기연 <sonky740@minwise.co.kr>
2022-04-05 15:03:30 +0900

    122~~ useReducer

--- 01-starting-project/src/index.js
+++ 01-starting-project/src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
-import './index.scss';
+import './index.css';
 import App from './App';
 const container = document.getElementById('root');
 const root = createRoot(container);
