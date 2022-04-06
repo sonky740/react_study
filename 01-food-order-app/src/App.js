1804bd6875593453150e5d8c68e07b97d90747d3
Sonky <sonky740@gmail.com>
2022-04-06 23:09:29 +0900

    146~~ 143~~ modal

--- 01-food-order-app/src/App.js
+++ 01-food-order-app/src/App.js
@@ -1,10 +1,24 @@
+import React, { useState } from 'react';
+import Cart from './components/Cart/Cart';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
