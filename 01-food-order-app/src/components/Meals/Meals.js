a906912b9a18f9ae1ae073d8f502a1daaa0cffc1
손기연 <sonky740@minwise.co.kr>
2022-04-06 16:42:18 +0900

    143~~

---
+++ 01-food-order-app/src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummaryfrom from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummaryfrom />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
