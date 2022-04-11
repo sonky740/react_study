acdcd035f346698d742b599bc6261285b324485d
손기연 <sonky740@minwise.co.kr>
2022-04-11 13:56:12 +0900

    156~~

--- 01-food-order-app/src/components/Layout/HeaderCartButton.js
+++ 01-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,23 +1,59 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css'
+import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const btn = useRef();
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
-  return ( <button className={classes.button} onClick={props.onClick}>
-    <span className={classes.icon}>
-      <CartIcon />
-    </span>
-    <span>Your Cart</span>
-    <span className={classes.badge}>{numberOfCartItems}</span>
-  </button> );
-} 
+  const btnClasses = `${classes.button}${
+    btnIsHighlighted ? ` ${classes.bump}` : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    btn.current.addEventListener(
+      'animationend',
+      () => {
+        setBtnIsHighlighted(false);
+        console.log('a');
+      },
+      {
+        once: true,
+      }
+    );
+
+    // const timer = setTimeout(() => {
+    //   setBtnIsHighlighted(false);
+    // }, 300)
+    
+    // return () => {
+    //   clearTimeout(timer);
+    // }
+  }, [items]);
+
+  return (
+    <button className={btnClasses} ref={btn} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
 
 export default HeaderCartButton;
--- 01-food-order-app/src/store/CartProvider.js
+++ 01-food-order-app/src/store/CartProvider.js
@@ -11,14 +11,16 @@ const cartReducer = (state, action) => {
     const updateTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
-    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
-        amount: existingCartItem.amount + action.item.amount
+        amount: existingCartItem.amount + action.item.amount,
       };
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
@@ -28,6 +30,25 @@ const cartReducer = (state, action) => {
     return {
       items: updatedItems,
       totalAmount: updateTotalAmount,
+    };
+  }
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
   }
   return defaultCartState;
