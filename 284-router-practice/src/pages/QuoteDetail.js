dbe67f1a1620ab69022eec5bb6e87a5d069b4040
손기연 <sonky740@minwise.co.kr>
2022-06-09 17:17:31 +0900

    295~~

--- 284-router-practice/src/pages/QuoteDetail.js
+++ 284-router-practice/src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { useParams, Route } from 'react-router-dom';
+import { useParams, Route, Link } from 'react-router-dom';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
@@ -19,15 +19,22 @@ const DUMMY_QUOTES = [
 const QuoteDetail = () => {
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if(!quote) {
-    return <p>No quote found!</p>
+  if (!quote) {
+    return <p>No quote found!</p>;
   }
 
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
