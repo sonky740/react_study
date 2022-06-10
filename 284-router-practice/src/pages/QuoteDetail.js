f636c4882385742a2d1b20c47c28344cad1224b4
손기연 <sonky740@minwise.co.kr>
2022-06-10 13:57:54 +0900

    297~~

--- 284-router-practice/src/pages/QuoteDetail.js
+++ 284-router-practice/src/pages/QuoteDetail.js
@@ -1,7 +1,12 @@
-import { useParams, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+import usehttp from '../hooks/use-http';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
 const DUMMY_QUOTES = [
   {
@@ -17,25 +22,42 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
+  const { quoteId } = params;
+  const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if (!quote) {
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId])
+
+  if (status === 'pending') {
+    return <div className="centered">
+      <LoadingSpinner />
+    </div>
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </>
