c3af3d279714919912c2701696c4c92ea1553e74
손기연 <sonky740@minwise.co.kr>
2022-06-17 12:08:13 +0900

    388~~~

--- 374-redux-hook/src/containers/Favorites.js
+++ 374-redux-hook/src/containers/Favorites.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 import FavoriteItem from '../components/Favorites/FavoriteItem';
+import { useStore } from '../hooks-store/store';
 import './Products.css';
 
-const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
+const Favorites = () => {
+  const state = useStore()[0];
+  const favoriteProducts = state.products.filter(p => p.isFavorite);
+
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
       <ul className="products-list">
-        {favoriteProducts.map(prod => (
+        {favoriteProducts.map((prod) => (
           <FavoriteItem
             key={prod.id}
             id={prod.id}
--- 374-redux-hook/src/index.js
+++ 374-redux-hook/src/index.js
@@ -1,24 +1,19 @@
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+import configureProductsStore from './hooks-store/products-store';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+configureProductsStore();
+
 root.render(
-  <Provider store={store}>
+  <>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </>
 );
