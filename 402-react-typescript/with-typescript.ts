35b1637d53397ae0e191f51fc94064b4ebf40eb6
손기연 <sonky740@minwise.co.kr>
2022-06-23 17:00:49 +0900

    411~~

--- 402-react-typescript/with-typescript.ts
+++ 402-react-typescript/with-typescript.ts
@@ -1,7 +1,45 @@
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+let person: Person;
+
+person = {
+  name: 'Max',
+  age: 32,
+};
+
+let people: Person[];
+
+people = [
+  { name: 'Max', age: 32 },
+  { name: 'Min', age: 14 },
+];
+
+// Functions & types
+
 function add(a: number, b: number) {
   return a + b;
 }
 
-const result = add(2, 5);
+function printOutput(value: any) {
+  console.log(value);
+}
 
-console.log(result);
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+
+updatedArray[0].split('');
