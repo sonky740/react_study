1ea9ef94238ec04a4d6bd473ab49de0e7d4847b9
Sonky <sonky740@gmail.com>
2023-02-14 00:07:46 +0900

    jest: 기본 개념

---
+++ jest/color-button/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <button type="button" style={{ backgroundColor: 'red' }}>Change to blue</button>
+    </div>
+  );
+}
+
+export default App;
