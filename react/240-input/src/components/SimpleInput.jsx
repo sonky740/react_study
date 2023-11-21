143df2a2d5374f2f1917ef80f76cb9dd3a93ce9d
Sonky <sonky740@gmail.com>
2023-11-21 22:16:33 +0900

    섹션 17 244~

---
+++ react/240-input/src/components/SimpleInput.jsx
@@ -0,0 +1,52 @@
+import { useRef, useState } from 'react';
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    
+    setEnteredName('');
+    setEnteredNameIsValid(true);
+  };
+
+  const nameInputClasses = enteredNameIsValid
+    ? 'form-control'
+    : 'form-control invalid';
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        {!enteredNameIsValid && <p>Name must not be empty.</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
