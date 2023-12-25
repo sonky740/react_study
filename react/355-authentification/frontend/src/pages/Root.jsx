c9ddcb546db086b5a7bca2fd263f06401e26faf6
손기연 <mykiller4@naver.com>
2023-12-25 21:12:33 +0900

    섹션 24

---
+++ react/355-authentification/frontend/src/pages/Root.jsx
@@ -0,0 +1,39 @@
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
+
+import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../utils/auth';
+
+function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'POST' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'POST' });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
